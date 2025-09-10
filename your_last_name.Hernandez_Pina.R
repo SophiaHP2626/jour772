302ecbb45dc671236be56c5090a12d12dfa7db3f
SophiaHP2626 <sophia41202@gmail.com>
2025-09-09 22:40:28 -0400

    pre_lab_01.rmd

---
+++ your_last_name.Hernandez_Pina.R
@@ -0,0 +1,2 @@
+write a message with your name, expressing joy and relief
+Hello, my name is Sophia Hernandez-Piña, I am very excited and honestly relieved I somehow managed to get into Graduate School.
